ef7a8a56f3e2f9fed051291d4504c31440bdf409
Luis Almeida <lf.mc.almeida@poli.ufrj.br>
2022-04-02 22:08:46 -0300

    projeto-driveneats-V3.0_sembonus

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function selecionar(elemento,tipoItem) {
-    const setado = document.querySelector("." +tipoItem);
+function botaoliberado() {
     const botao = document.querySelector(".botaofinalizar");
     const inicial = '<p>Selecione os 3 itens <br></br> para fechar o pedido</p>';
+    if ((document.querySelector(".selecionado-principal"))&&(document.querySelector(".selecionado-bebida"))&&document.querySelector(".selecionado-sobremesa")){
+        botao.classList.add("botaoliberado");
+        botao.innerHTML = "Finalizar o Pedido";
+        return true;
+    }else{
+        botao.classList.remove("botaoliberado");
+        botao.innerHTML = inicial;
+        return false;
+    }
     
+}
+
+function selecionar(elemento,tipoItem) {
+    const setado = document.querySelector("." +tipoItem);
     if(setado !== null){
         setado.classList.remove(tipoItem);
     }
@@ -11,12 +23,29 @@ function selecionar(elemento,tipoItem) {
     }else {
         elemento.classList.add(tipoItem);
     }
-    
-    if ((document.querySelector(".selecionado-principal"))&&(document.querySelector(".selecionado-bebida"))&&document.querySelector(".selecionado-sobremesa")){
-        botao.classList.add("botaoliberado");
-        botao.innerHTML = "Finalizar o Pedido";
-    }else{
-        botao.classList.remove("botaoliberado");
-        botao.innerHTML = inicial;
+    botaoliberado(); 
+}
+
+
+
+function mensagemWp() {
+    if(botaoliberado()){
+        const prato = document.querySelector(".selecionado-principal h3").innerHTML;
+        const bebida = document.querySelector(".selecionado-bebida h3").innerHTML;
+        const sobremesa = document.querySelector(".selecionado-sobremesa h3").innerHTML;
+        let total = Number(document.querySelector(".selecionado-principal h4").innerHTML.replace('R$','').replace(',','.')) +
+         Number(document.querySelector(".selecionado-bebida h4").innerHTML.replace('R$','').replace(',','.')) + 
+         Number(document.querySelector(".selecionado-sobremesa h4").innerHTML.replace('R$','').replace(',','.'));
+         alert(total);
+
+        const mensagem = `Olá, gostaria de fazer o pedido:
+- Prato: ${prato}
+- Bebida: ${bebida}
+- Sobremesa: ${sobremesa}
+Total: R$ ${total.toFixed(2)}`;
+        alert(mensagem);
+
+        window.open("https:/wa.me/5571981086378?text=" + window.encodeURIComponent(mensagem));
     }
+    
 }
